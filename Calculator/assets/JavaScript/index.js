0bc359abae98fe56a72deee4cf8a960e7d99f20e
Mohit <aumento.dev110@gmail.com>
2023-06-06 16:17:26 +0530

    Update(calculator)

--- Calculator/assets/JavaScript/index.js
+++ Calculator/assets/JavaScript/index.js
@@ -1,11 +1,11 @@
-document.addEventListener("paste", function(event) {
+document.addEventListener("paste", function (event) {
   const element = event.target;
 
   if (element.tagName === "INPUT" && element.type === "text") {
     const clipboardData = event.clipboardData || window.clipboardData;
     const pastedText = clipboardData.getData("text/plain").trim();
 
-    if (!/^[0-9+\-*/%.]+$/.test(pastedText)) {
+    if (!/^\d+(\s*[-+\/*%]\s*\d+)*$/.test(pastedText)) {
       event.preventDefault();
     } else {
       const currentValue = element.value;
@@ -14,13 +14,13 @@ document.addEventListener("paste", function(event) {
 
       const newValue =
         currentValue.substring(0, selectionStart) +
-        pastedText +
+        addSpacesToExpression(pastedText) +
         currentValue.substring(selectionEnd);
 
       element.value = newValue;
       element.setSelectionRange(
-        selectionStart + pastedText.length,
-        selectionStart + pastedText.length
+        selectionStart + pastedText.length + countSpaces(pastedText),
+        selectionStart + pastedText.length + countSpaces(pastedText)
       );
 
       event.preventDefault();
@@ -28,17 +28,58 @@ document.addEventListener("paste", function(event) {
   }
 });
 
+function addSpacesToExpression(expression) {
+  return expression.replace(/(\d+|[-+\/*%]+)/g, '$1 ');
+}
+
+function countSpaces(expression) {
+  return (expression.match(/(\d+)([-+\/*%])(\d+)/g) || []).length;
+}
+
+
+let isNewCalculation = false;
 function display(value) {
   const isNumber = /^\d+$/.test(value);
   const showcase = document.getElementById("showcase");
   const lastChar = showcase.value.slice(-1);
+  const operators = ['+', '-', '*', '/', '%'];
+
+  if (value === '%' && showcase.value === '') {
+    return;
+  } else if (value === '+' && showcase.value === '') {
+    return;
+  } else if (value === '-' && showcase.value === '') {
+    return;
+  } else if (value === '*' && showcase.value === '') {
+    return;
+  } else if (value === '/' && showcase.value === '') {
+    return;
+  }
 
   if (isNumber) {
-    showcase.value += value;
-  } else if (value === '+' || value === '-' || value === '*' || value === '/' || value === '%') {
-    if (lastChar !== ' ' && lastChar !== '') {
-      showcase.value += ' ' + value + ' ';
+    if (lastChar === '%') {
+        showcase.value += ' ' + value;
+    } 
+    else if (isNewCalculation) {
+      if(lastChar !== ' '){
+        showcase.value = value;
+      }
+      isNewCalculation = false;
+    } else  {
+        showcase.value += value;
+    } 
+  } else if (operators.includes(value)) {
+    const expression = showcase.value.trim();
+    const tokens = expression.split(' ');
+    const lastToken = tokens[tokens.length - 1];
+
+    if (operators.includes(lastToken)) {
+      tokens[tokens.length - 1] = value + ' ';
+    } else {
+      tokens.push(value + ' ');
     }
+
+    showcase.value = tokens.join(' ');
   } else if (value === '+/-') {
     const expression = showcase.value.trim();
     const tokens = expression.split(' ');
@@ -75,34 +116,37 @@ function display(value) {
 
     showcase.value = tokens.join(' ');
   } else if (value === '.' || value === 'Decimal') {
-    const expression = showcase.value.trim();
-    const lastNumber = getLastNumber(expression);
+    const lastNumber = getLastNumber(showcase.value);
 
-    // Check if lastNumber already contains a decimal point
-    if (!lastNumber.includes('.') && lastNumber !== '') {
+    if (lastNumber === '' || lastNumber === '-') {
+      showcase.value += '0.';
+    } else if (!lastNumber.includes('.')) {
       showcase.value += '.';
     }
   }
 }
 
 
+
 function clearScreen() {
   document.getElementById("showcase").value = "";
+  isNewCalculation = false;
 }
 
 function calculate() {
   const expression = document.getElementById("showcase").value;
   const result = evaluateExpression(expression);
   document.getElementById("showcase").value = result;
+  isNewCalculation = true;
 }
 
 function evaluateExpression(expression) {
-  const tokens = expression.split(' ');
+  const tokens = expression.split(" ");
 
   // Convert '-' to '@' to handle negative numbers
   for (let i = 0; i < tokens.length; i++) {
-    if (tokens[i] === '-') {
-      tokens[i] = '@';
+    if (tokens[i] === "-") {
+      tokens[i] = "@";
     }
   }
 
@@ -110,28 +154,35 @@ function evaluateExpression(expression) {
   let incompleteExpression = false;
 
   for (let i = 1; i < tokens.length; i += 2) {
-    const operator = tokens[i].replace('@', '-');
+    const operator = tokens[i].replace("@", "-");
     const num = parseFloat(tokens[i + 1]);
 
-    if (operator === '+' && !isNaN(num)) {
+    if (operator === "+" && !isNaN(num)) {
       result += num;
       incompleteExpression = false;
-    } else if (operator === '-' && !isNaN(num)) {
+    } else if (operator === "-" && !isNaN(num)) {
       result -= num;
       incompleteExpression = false;
-    } else if (operator === '*' && !isNaN(num)) {
+    } else if (operator === "*" && !isNaN(num)) {
       result *= num;
       incompleteExpression = false;
-    } else if (operator === '/' && !isNaN(num)) {
+    } else if (operator === "/" && !isNaN(num)) {
       result /= num;
       incompleteExpression = false;
-    } else if (operator === '%') {
+    } else if (operator === "%") {
       if (isNaN(num)) {
         result /= 100;
       } else {
         result = (result * num) / 100;
       }
       incompleteExpression = false;
+    } else if (operator === "+/-" && isNaN(num)) {
+      result = -result;
+      incompleteExpression = false;
+    } else if (operator === "+/-" && !isNaN(num)) {
+      const lastNumber = getLastNumber(expression);
+      const newExpression = expression.replace(lastNumber, "-" + lastNumber);
+      return evaluateExpression(newExpression);
     } else {
       incompleteExpression = true;
     }
@@ -143,7 +194,6 @@ function evaluateExpression(expression) {
     return result;
   }
 }
- 
 
 function getLastNumber(expression) {
   const tokens = expression.split(' ');
@@ -158,6 +208,7 @@ function getLastNumber(expression) {
   return '';
 }
 
+
 function removeLastElement() {
   const showcase = document.getElementById("showcase");
   const expression = showcase.value.trim();
@@ -169,37 +220,61 @@ function removeLastElement() {
   showcase.value = expression.slice(0, -1);
 }
 
-document.getElementById("showcase").addEventListener("keydown", function(event) {
+document.getElementById("showcase").addEventListener("keydown", function (event) {
   const keyPressed = event.key;
-
   if (keyPressed === 'Backspace') {
     removeLastElement();
     event.preventDefault();
   } else if (keyPressed === 'Enter') {
     calculate();
     event.preventDefault();
+  } 
+  if (!/^[0-9+\-*/%.]$/.test(keyPressed)) {
+    event.preventDefault();
   } else {
-    if (keyPressed === '+') {
-      display('+');
+    if (keyPressed === "+") {
+      display("+");
       event.preventDefault();
-    } else if (keyPressed === '-') {
-      display('-');
+    } else if (keyPressed === "-") {
+      display("-");
       event.preventDefault();
-    } else if (keyPressed === '*') {
-      display('*');
+    } else if (keyPressed === "*") {
+      display("*");
       event.preventDefault();
-    } else if (keyPressed === '/') {
-      display('/');
+    } else if (keyPressed === "/") {
+      display("/");
       event.preventDefault();
-    } else if (keyPressed === '%') {
-      display('%');
+    } else if (keyPressed === "%") {
+      const lastChar = document.getElementById("showcase").value.slice(-1);
+      if (/^\d$/.test(lastChar)) {
+        display("%");
+      }
       event.preventDefault();
-    } else if (keyPressed === '+/-') {
-      display('+/-');
+    } else if (keyPressed === "+/-") {
+      display("+/-");
       event.preventDefault();
-    } else if (keyPressed === '.' || keyPressed === 'Decimal') {
-      display('.');
+    } else if (keyPressed === "." || keyPressed === "Decimal") {
+      display(".");
       event.preventDefault();
     }
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
